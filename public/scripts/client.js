43f0ac70389fa321e127a71374ba18a6a1f14f34
knoori-code <knoori3@gmail.com>
2023-02-01 18:21:33 -0500

    Create loadTweets function to get tweets and render on webpage

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-  const tweetData = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   const createTweetElement = function (tweetObj) {
     const tweetArticle = `
   <article>
@@ -57,11 +32,20 @@ $(document).ready(function () {
     }
   };
 
-  renderTweets(tweetData);
+  const loadTweets = function () {
+    $.get("/tweets", function (data) {
+      renderTweets(data);
+      console.log(data)
+    });
+  };
+
+  // renderTweets(tweetData);
+
+  loadTweets()
 
   $(".form").on("submit", function (event) {
     event.preventDefault();
     const string = $(this).serialize();
     $.post("/tweets", string);
-  })
-})
+  });
+});
